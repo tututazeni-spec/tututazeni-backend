4435e577cdbdd0d3a5168570d881fc4d1e643e59
Placido Costa <evoslda2025@gmail.com>
2026-03-21 15:33:19 +0100

    Primeiro commit - projeto NestJS

--- src/core/auth/auth.service.spec.ts
+++ src/core/auth/auth.service.spec.ts
@@ -1,12 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { PrismaService } from '../../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const mockPrisma = {
+    user: {
+      findUnique: jest.fn(),
+      create: jest.fn(),
+    },
+    role: {
+      findUnique: jest.fn(),
+      create: jest.fn(),
+    },
+  };
+
+  const mockJwt = {
+    signAsync: jest.fn().mockResolvedValue('token'),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: PrismaService,
+          useValue: mockPrisma,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwt,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -16,3 +43,4 @@ describe('AuthService', () => {
     expect(service).toBeDefined();
   });
 });
+
