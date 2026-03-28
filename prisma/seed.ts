fc90d6cb0bc3247d9dbb9920fab20bf351324d81
Placido Costa <evoslda2025@gmail.com>
2026-03-28 19:06:11 +0100

    feat: adicionar seed com utilizador admin

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,9 +2,6 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 import * as bcrypt from 'bcrypt';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
